39657b6e1c94bffea902ed53f63bd598666104a4
Rashad Sirajudeen <rashad@wso2.com>
2026-03-14 13:39:10 +0530

    docs: fix observability helm chart references
    - Replace openchoreo/charts with openchoreo/helm-charts (correct registry path)
    - Rename observability-traces-opensearch to observability-tracing-opensearch (correct chart name)
    - Use CodeBlock component for version-templated helm command in k3d docs
    
    Signed-off-by: Rashad Sirajudeen <rashad@wso2.com>

--- docs/getting-started/try-it-out/on-k3d-locally/k3d-observability-plane.sh
+++ docs/getting-started/try-it-out/on-k3d-locally/k3d-observability-plane.sh
@@ -18,15 +18,15 @@ helm upgrade --install openchoreo-observability-plane oci://ghcr.io/openchoreo/h
 
 step "Installing OpenSearch-based logs module..."
 helm upgrade --install observability-logs-opensearch \
-  oci://ghcr.io/openchoreo/charts/observability-logs-opensearch \
+  oci://ghcr.io/openchoreo/helm-charts/observability-logs-opensearch \
   --create-namespace \
   --namespace openchoreo-observability-plane \
   --version 0.3.8 \
   --set openSearchSetup.openSearchSecretName="opensearch-admin-credentials"
 
 step "Installing OpenSearch-based traces module..."
-helm upgrade --install observability-traces-opensearch \
-  oci://ghcr.io/openchoreo/charts/observability-traces-opensearch \
+helm upgrade --install observability-tracing-opensearch \
+  oci://ghcr.io/openchoreo/helm-charts/observability-tracing-opensearch \
   --create-namespace \
   --namespace openchoreo-observability-plane \
   --version 0.3.6 \
@@ -35,14 +35,14 @@ helm upgrade --install observability-traces-opensearch \
 
 step "Installing Prometheus-based metrics module..."
 helm upgrade --install observability-metrics-prometheus \
-  oci://ghcr.io/openchoreo/charts/observability-metrics-prometheus \
+  oci://ghcr.io/openchoreo/helm-charts/observability-metrics-prometheus \
   --create-namespace \
   --namespace openchoreo-observability-plane \
   --version 0.2.4
 
 step "Enabling logs collection in the configured logs module..."
 helm upgrade observability-logs-opensearch \
-  oci://ghcr.io/openchoreo/charts/observability-logs-opensearch \
+  oci://ghcr.io/openchoreo/helm-charts/observability-logs-opensearch \
   --namespace openchoreo-observability-plane \
   --version 0.3.8 \
   --reuse-values \
